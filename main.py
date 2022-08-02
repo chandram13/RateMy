7c8b5aac399d096f5ee8bb4e395d815a1b24f4fe
Marvish <mschandr@uci.edu>
2022-08-02 14:04:43 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Marvish Chandra
+
+'''Through web scraping, this program is intended to help students find the rating information of a college from the Rate My Professor database.'''
+
+def findInfo:
+
+
